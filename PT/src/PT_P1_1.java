64c225e69dd618b7b535ef979cd9b6c8f70342cc
Hendrik Klein-Hitpass <Henni.kleinh@gmail.com>
2018-11-09 03:08:56 +0100

    adddoc

--- PT/src/PT_P1_1.java
+++ PT/src/PT_P1_1.java
@@ -1,8 +1,11 @@
 
 public class PT_P1_1 {
 	PT_P1_1(){
+		
 		long l=(long)Math.pow(3, 22);
 		int i = (int)l;
+		
+		System.out.println(l);
 		System.out.println(i);
 	}
 	public static void main(String[] args) {
--- PT/src/PT_P1_2.java
+++ PT/src/PT_P1_2.java
@@ -40,7 +40,6 @@ public class PT_P1_2 {
 			}
 			//tauschen
 			//eliminieren
-			
 		}
 	}
 }
