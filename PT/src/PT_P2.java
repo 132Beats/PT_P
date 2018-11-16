284cf00cc1e8dbaff8ccb2bf5f66abe9e710a592
Hendrik Klein-Hitpass <Henni.kleinh@gmail.com>
2018-11-16 12:52:51 +0100

    nicer dicer

--- PT/src/PT_P2.java
+++ PT/src/PT_P2.java
@@ -1,21 +1,22 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 public class PT_P2 {
-
 	public static void main(String[] args) {
 		GregorianCalendar greg=new GregorianCalendar();
 		int day_of_week=greg.get(Calendar.DAY_OF_WEEK);
 		int counter=0;
 		if(greg.get(Calendar.ZONE_OFFSET)==3600000) {
 			day_of_week=(day_of_week+6)%7;
+			System.out.println("Mo    Di    Mi    Do    Fr    Sa    So");
 		}
-		//System.out.println((day_of_week-((greg.get(Calendar.DAY_OF_MONTH)%7))+7)%7);
+		else{
+			System.out.println("Su    Mo    Tu    We    Th    Fr    Sa");
+		}
 		for(int i=0;i<(day_of_week-((greg.get(Calendar.DAY_OF_MONTH)%7))+7)%7;i++) {
 			System.out.print("      ");
 			counter++;
 		}
 		for(int i=0;i<greg.getActualMaximum(Calendar.DAY_OF_MONTH);i++) {
-			//System.out.print("("+counter+")");
 			if (counter>=7) {
 				counter=counter-7;
 				System.out.print("\n");
@@ -32,11 +33,5 @@ public class PT_P2 {
 			}
 			counter++;
 		}
-		//System.out.println("");
-		//System.out.println(day_of_week);
-		//System.out.println(greg.getActualMaximum(Calendar.DAY_OF_MONTH));
-		//System.out.println(greg.get(Calendar.DAY_OF_MONTH));
-		//System.out.println(greg.get(Calendar.ZONE_OFFSET));
 	}
-
 }
